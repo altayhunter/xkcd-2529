637aff62abf06d8b402b319ee719c0a32980613b
Altay Hunter <altayhunter@gmail.com>
2021-11-30 21:25:48 -0500

    Move duplicated code into average function.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,22 +4,26 @@
 
 using namespace std;
 
+double average(const vector<uint64_t>& v) {
+	uint64_t sum = accumulate(v.begin(), v.end(), 0);
+	return static_cast<double>(sum) / v.size();
+}
+
 int main() {
 	unsigned n = 4;
 	unsigned k = 1000;
+	unsigned runs = 1000000;
 	vector<uint64_t> steps;
 	vector<uint64_t> intersections;
-	for (int i = 0; i < 1000000; i++) {
+	steps.reserve(runs);
+	intersections.reserve(runs);
+	for (int i = 0; i < runs; i++) {
 		Walker w(n, k);
 		steps.push_back(w.steps());
 		intersections.push_back(w.intersections());
 	}
-	double avgSteps =
-			1.0 * accumulate(steps.begin(), steps.end(), 0) / steps.size();
-	double avgIntersections =
-			1.0 * accumulate(intersections.begin(), intersections.end(), 0) /
-			intersections.size();
-	cout << "Average of " << steps.size() << " runs is " << avgSteps
-			<< " steps and " << avgIntersections << " intersections" << endl;
+	cout << "Average of " << runs << " runs is " << average(steps)
+			<< " steps and " << average(intersections)
+			<< " intersections" << endl;
 	return 0;
 };
