b3d719b30cfbfa415de506f7c9d0c84fcd85304e
Altay Hunter <altayhunter@gmail.com>
2023-02-25 10:26:36 -0500

    Optimize the random number generation by avoiding stepping backwards. After the first step there are only three legal directions of travel, so by generating four numbers we end up having to reroll 25% of the time. With this change we just generate three numbers instead, reducing the necessary rerolls considerably, improving overall performance by ~23%. This also fixes a minor bug in the original implementation where a marble would never be placed at the location where the walker traps itself.

--- rust/src/walker.rs
+++ rust/src/walker.rs
@@ -10,6 +10,14 @@ pub struct Walker {
 	visited: HashSet<Point>,
 }
 
+#[derive(Clone, Copy, Debug)]
+enum Direction {
+	Up,
+	Right,
+	Down,
+	Left,
+}
+
 impl Walker {
 	pub fn new(rng: &mut SmallRng, n: usize, k: usize) -> Self {
 		let mut location = Point::new(0, 0);
@@ -18,14 +26,20 @@ impl Walker {
 		if n == 0 || k == 0 {
 			return Walker {marbles, visited};
 		}
+		let mut direction;
+		(location, direction) = Self::first_random_neighbor(rng, location);
+		visited.insert(location);
+		if n == 1 {
+			marbles.push(location);
+			if k == 1 { return Walker {marbles, visited}; }
+		}
 		while !Self::trapped(&visited, location) {
-			let desired = Self::valid_neighbor(rng, &visited, location);
-			if visited.len() % n == 0 {
-				marbles.push(desired);
+			(location, direction) =
+					Self::valid_neighbor(rng, &visited, location, direction);
+			visited.insert(location);
+			if (visited.len() - 1) % n == 0 {
+				marbles.push(location);
 			}
-			visited.insert(desired);
-			location = desired;
-			log::debug!("Visited {}", location);
 			if visited.len() > n * k {
 				break;
 			}
@@ -44,19 +58,48 @@ impl Walker {
 		visited.contains(&p.down()) &&
 		visited.contains(&p.left())
 	}
-	fn valid_neighbor(rng: &mut SmallRng, visited: &HashSet<Point>, p: Point) -> Point {
-		let mut neighbor = Self::random_neighbor(rng, p);
-		while visited.contains(&neighbor) {
-			neighbor = Self::random_neighbor(rng, p);
+	fn valid_neighbor(
+			rng: &mut SmallRng,
+			visited: &HashSet<Point>,
+			p: Point,
+			d: Direction) -> (Point, Direction) {
+		let mut neighbor = Self::random_neighbor(rng, p, d);
+		while visited.contains(&neighbor.0) {
+			neighbor = Self::random_neighbor(rng, p, d);
 		}
 		neighbor
 	}
-	fn random_neighbor(rng: &mut SmallRng, p: Point) -> Point {
+	fn first_random_neighbor(rng: &mut SmallRng, p: Point) -> (Point, Direction) {
 		match rng.gen_range(0..4) {
-			0 => p.up(),
-			1 => p.right(),
-			2 => p.down(),
-			_ => p.left(),
+			0 => (p.up(), Direction::Up),
+			1 => (p.right(), Direction::Right),
+			2 => (p.down(), Direction::Down),
+			_ => (p.left(), Direction::Left),
+		}
+	}
+	fn random_neighbor(rng: &mut SmallRng, p: Point, d: Direction)
+			-> (Point, Direction) {
+		match d {
+			Direction::Up => match rng.gen_range(0..3) {
+				0 => (p.left(), Direction::Left),
+				1 => (p.up(), Direction::Up),
+				_ => (p.right(), Direction::Right),
+			},
+			Direction::Right => match rng.gen_range(0..3) {
+				0 => (p.up(), Direction::Up),
+				1 => (p.right(), Direction::Right),
+				_ => (p.down(), Direction::Down),
+			},
+			Direction::Down => match rng.gen_range(0..3) {
+				0 => (p.right(), Direction::Right),
+				1 => (p.down(), Direction::Down),
+				_ => (p.left(), Direction::Left),
+			},
+			Direction::Left => match rng.gen_range(0..3) {
+				0 => (p.down(), Direction::Down),
+				1 => (p.left(), Direction::Left),
+				_ => (p.up(), Direction::Up),
+			},
 		}
 	}
 }
